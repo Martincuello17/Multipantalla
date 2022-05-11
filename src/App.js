02bfece1a366864bd1dcdac672fa82d53dcbf281
Martin Cuello <martin30016@gmail.com>
2022-05-11 20:46:08 -0300

    secondary commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hola desde Welcome!</h1>;
+  }
+}
+// const element = (
+//   <h1>Hola!</h1>
+// )
+
+export default App;
